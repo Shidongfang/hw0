087b1f4b88d074b21e36b4d97e590337b24dfa8d
Shidongfang <1584262607@qq.com>
2023-03-06 15:18:42 +0000

    hw0 first exercise

---
+++ hw0_1.sh
@@ -0,0 +1,32 @@
+# Basic Exercises
+# 1
+# echo "hello world" > /home/200860417/hw0/hello.txt
+# 2
+# mkdir hw0_2
+# 3
+# rmdir hw0_2
+# 4
+# chmod a+r hello.txt
+# 5
+
+# 6
+# find . -name "hello"
+# 7
+# ll > directry_info
+# 8
+# cat hello world > hello_world
+# 9
+# ls | grep hello
+# 10
+# ls /usr/bin | less
+# 11
+# cd ..
+# vim .bashrc
+# alias ll="ls -al" 
+
+#Advance Exercises
+# 1
+# ls -a -l -h
+# 2
+# find . -name *.html | xargs -d '\n' zip html.zip
+
